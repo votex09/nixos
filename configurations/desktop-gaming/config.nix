81332ff7038ef170bfa79b623a22720a51686aa7
stixx <kylerjuresic@gmail.com>
2025-12-16 22:22:35 -0600

    fix imports

--- configurations/desktop-gaming/config.nix
+++ configurations/desktop-gaming/config.nix
@@ -2,8 +2,8 @@
 
 {
   imports = [
-    (self + /client/hardware-configuration.nix)
-    (self + /coresys/applications.nix)
+    ../../../client/hardware-configuration.nix
+    ../../../coresys/applications.nix
   ];
 
   # System settings from variables.nix
